a7111daab2883bbf005ebfd4f19a2f13e121f366
ikievite <ikievite@gmail.com>
2021-05-01 15:40:41 +0300

    added request_mock test

--- tests/scripts/test_page_loader.py
+++ tests/scripts/test_page_loader.py
@@ -3,8 +3,21 @@
 """Tests for page-loader package."""
 
 
-from page_loader.scripts.page_loader import main
+import pytest
+import tempfile
+import pathlib
+
+from page_loader.scripts.page_loader import download
 
 
-def test_main():
-    assert main() == 'Let\'s start.'
+@pytest.mark.skip(reason='deativated')
+def test_download_check_filepath():
+    assert download('https://ru.hexlet.io/courses', '/var/tmp') == '/var/tmp/ru-hexlet-io-courses.html'
+
+
+def test_download_check_content(requests_mock):
+    requests_mock.get('http://test.com', text='data')
+    with tempfile.TemporaryDirectory() as directory_name:
+        the_dir = pathlib.Path(directory_name)
+        with open(download('http://test.com', the_dir)) as f:
+            assert f.read() == 'data'
