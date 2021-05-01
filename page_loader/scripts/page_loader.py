cba7e038b679b8b25d320d8682e7becfdb2ee3f7
ikievite <ikievite@gmail.com>
2021-05-01 15:30:55 +0300

    updated start script

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -3,13 +3,18 @@
 """Page loader."""
 
 
-def main():
-    """Run main function.
+from page_loader.args_parser import parse_arguments
+from page_loader.downloader import RequestError, download
 
-    Returns:
-        test string
-    """
-    return "Let's start."
+
+def main():
+    """Run main function."""
+    args = parse_arguments()
+    try:  # noqa: WPS229 # ignore warning about too long `try` body length
+        page_content = download(args.url_path, args.output_dir)
+        print(page_content)  # noqa: WPS421 # ignore warning about print
+    except RequestError as e:  # noqa: WPS111 # ignore warning about too short name
+        print('Exception: {0}'.format(str(e)))  # noqa: WPS421 # ignore warning about print
 
 
 if __name__ == '__main__':
