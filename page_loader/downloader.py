4ebd5956c49f549cf05147140fc393afb77f546c
ikievite <ikievite@gmail.com>
2021-05-03 11:55:51 +0300

    added functions: download_file, mkdir, prepare_page

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -6,9 +6,11 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
 
 CHUNK_SIZE = 100000
-HTML_EXTENSION = 'html'
+HTML_EXTENSION = '.html'
+DIRECTORY_TRAILER = '_files'
 
 
 class RequestError(Exception):
@@ -17,18 +19,75 @@ class RequestError(Exception):
     pass  # noqa: WPS420, WPS604 # ignore wrong keyword: pass, incorrect node inside `class` body
 
 
-def format_url(path):
+def format_url(path, suffix):
     """Format an url path.
 
     Args:
         path: url path
+        suffix: suffix for name
 
     Returns:
         formatted url
     """
     path = path.split('//')[-1]
     path = path.replace('/', '-').replace('.', '-')
-    return '{path}.{extension}'.format(path=path, extension=HTML_EXTENSION)
+    return '{path}{extension}'.format(path=path, extension=suffix)
+
+
+def download_file(url, filename):
+    """Download file using `url`.
+
+    Args:
+        url: download link
+        filename: filename for file
+    """
+    response = requests.get(url)
+    with open(filename, 'wb') as f:  # noqa: WPS111 # ignore warning about too short name
+        for chunk in response.iter_content(CHUNK_SIZE):
+            f.write(chunk)
+
+
+def mkdir(dir_name, output_dir):
+    """Create directory.
+
+    Args:
+        dir_name: dir_name
+        output_dir: output_dir
+    """
+    directory_path = os.path.join(output_dir, dir_name)
+    try:
+        os.mkdir(directory_path)
+    except FileExistsError:
+        print('The directory `{0}` was previously created'.format(  # noqa: WPS421
+            directory_path,                                 # ignore warning about `print`
+        ))
+
+
+def prepare_page(url, output_dir):  # noqa: WPS210 # ignore warning about too many local variables
+    """Find, replace links to images.
+
+    Args:
+        url: url path
+        output_dir: output_dir
+
+    Returns:
+        tag soup
+    """
+    response = requests.get(url)
+
+    directory_name = format_url(url, DIRECTORY_TRAILER)
+
+    mkdir(directory_name, output_dir)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    for image in soup.find_all('img'):
+        image_src_url = image['src']
+        image_full_url = os.path.join(url, os.path.normpath(image_src_url))
+        image_path = os.path.join(directory_name, format_url(image_full_url, ''))
+        download_file(image_full_url, image_path)
+        soup.find(src=image_src_url)['src'] = image_path
+    return soup
 
 
 def download(url_path, output_dir):
@@ -41,14 +100,13 @@ def download(url_path, output_dir):
     Returns:
         formatted url path
     """
-    res = requests.get(url_path)
+    page_name = format_url(url_path, HTML_EXTENSION)
 
-    formatted_url = format_url(url_path)
+    page_filepath = os.path.join(output_dir, page_name)
 
-    formatted_path = os.path.join(output_dir, formatted_url)
+    saved_page = prepare_page(url_path, output_dir)
 
-    with open(formatted_path, 'wb') as f:  # noqa: WPS111 # ignore warning about too short name
-        for chunk in res.iter_content(CHUNK_SIZE):
-            f.write(chunk)
+    with open(page_filepath, 'w') as f:  # noqa: WPS111 # ignore warning about too short name
+        f.write(str(saved_page.prettify(formatter='html5')))
 
-    return formatted_path
+    return page_filepath
