4fe33fecfb9ee888953a7ee37eecbc89ba083e80
ikievite <ikievite@gmail.com>
2021-05-03 11:57:03 +0300

    added tests

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -3,8 +3,46 @@
 """Test downloader module."""
 
 
-from page_loader.downloader import format_url
+import os
+import pathlib
+import tempfile
+
+import bs4
+
+import requests_mock
+from page_loader.downloader import (HTML_EXTENSION, download, download_file,
+                                    format_url)
 
 
 def test_format_url():
-    assert format_url('https://ru.hexlet.io/courses') == 'ru-hexlet-io-courses.html'
+    assert format_url('https://ru.hexlet.io/courses', HTML_EXTENSION) == 'ru-hexlet-io-courses.html'
+
+
+def test_download_file(sample_file='tests/fixtures/original.original'):
+    with open(sample_file, 'rb') as f:
+        sample_file = f.read()
+    dl_path = 'http://test.com/thefile.file'
+
+    with requests_mock.Mocker() as mock:
+        # Return any size (doesn't matter, only for prints)
+        mock.head(requests_mock.ANY, headers={'Content-Length': '100'})
+
+        mock.get(dl_path, content=sample_file)
+
+        with tempfile.TemporaryDirectory() as directory_name:
+            the_dir = pathlib.Path(directory_name)
+            target_path = os.path.join(the_dir, 'test.file')
+            download_file(dl_path, target_path)
+
+            assert os.path.isfile(target_path)
+
+            with open(target_path, 'rb') as f:
+                assert f.read() == sample_file
+
+
+def test_download_check_content(requests_mock):
+    requests_mock.get('http://test.com', text='data')
+    with tempfile.TemporaryDirectory() as directory_name:
+        the_dir = pathlib.Path(directory_name)
+        with open(download('http://test.com', the_dir)) as f:
+            assert bs4.BeautifulSoup(f, 'lxml').get_text().strip() == 'data'
