5c4ea7dd394b05c2ddb455882f7d5d4c9637a0db
ikievite <ikievite@gmail.com>
2021-05-04 23:41:08 +0300

    added logging config to `__init__.py`

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,3 +1,19 @@
 
 
 """Page Loader package."""
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '{asctime} - {levelname} - {name} - {message}', datefmt='%H:%M:%S', style='{',
+)
+
+console.setFormatter(formatter)
+
+logger.addHandler(console)
