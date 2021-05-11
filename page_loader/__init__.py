1741ae5d730c66d30783ee2bdc6fcf0e60c51076
ikievite <ikievite@gmail.com>
2021-05-11 23:14:50 +0300

    added loggig to file

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -8,8 +8,9 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+### stderr
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+console.setLevel(logging.WARNING)
 formatter = logging.Formatter(
     '{asctime} - {levelname} - {name} - {message}', datefmt='%H:%M:%S', style='{',
 )
@@ -17,3 +18,13 @@ formatter = logging.Formatter(
 console.setFormatter(formatter)
 
 logger.addHandler(console)
+
+### File
+logfile = logging.FileHandler('logfile.log')
+logfile.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '{asctime} - {levelname} - {name} - {message}', datefmt='%H:%M:%S', style='{',
+)
+logfile.setFormatter(formatter)
+
+logger.addHandler(logfile)
