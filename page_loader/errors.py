df5fcb1e1104e5e171e9d4c1f8aa751e421ee77b
ikievite <ikievite@gmail.com>
2021-05-12 16:42:37 +0300

    solved linter issues with WPS420, WPS604 warnings.

--- page_loader/errors.py
+++ page_loader/errors.py
@@ -6,16 +6,16 @@
 class AppInternalError(Exception):
     """A class to represent app error."""
 
-    pass  # noqa: WPS420, WPS604 # ignore wrong keyword: pass, incorrect node inside `class` body
+    pass
 
 
 class RequestError(AppInternalError):
     """A class to represent network error."""
 
-    pass  # noqa: WPS420, WPS604 # ignore wrong keyword: pass, incorrect node inside `class` body
+    pass
 
 
 class FileError(AppInternalError):
     """A class to represent file error."""
 
-    pass  # noqa: WPS420, WPS604 # ignore wrong keyword: pass, incorrect node inside `class` body
+    pass
