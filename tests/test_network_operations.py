4f8c5f77fd675176af6f4dd3c5ab6dacaca7d86e
ikievite <ikievite@gmail.com>
2021-05-16 22:53:45 +0300

    solved linter`s issues

--- tests/test_network_operations.py
+++ tests/test_network_operations.py
@@ -5,7 +5,7 @@
 
 import pytest
 import requests
-import requests_mock
+import requests_mock  # noqa: F401
 from page_loader.helpers import get_content
 from page_loader.errors import RequestError
 
@@ -15,14 +15,15 @@ test_data = [
     (503, '503 Server Error'),
 ]
 
+
 @pytest.mark.parametrize("code,description", test_data)
-def test_get_content_codes(requests_mock, code, description):
+def test_get_content_codes(requests_mock, code, description):  # noqa: F811
     requests_mock.get('http://test.com', status_code=code)
-    with pytest.raises(RequestError, match=description) as exc_info:
+    with pytest.raises(RequestError, match=description):
         get_content('http://test.com')
 
 
-def test_get_content_exception(requests_mock):
+def test_get_content_exception(requests_mock):  # noqa: F811
     requests_mock.get('http://test.com', exc=requests.exceptions.ConnectTimeout)
-    with pytest.raises(RequestError) as exc_info:
+    with pytest.raises(RequestError):
         get_content('http://test.com')
