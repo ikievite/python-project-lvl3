c1231dc8720d4872603b09c003c58728c9089111
ikievite <ikievite@gmail.com>
2021-05-17 23:50:25 +0300

    removed useless test

--- tests/scripts/test_page_loader.py
+++ tests/scripts/test_page_loader.py
@@ -4,22 +4,7 @@
 
 
 import subprocess
-import pathlib
 import tempfile
-
-import bs4
-
-import pytest
-from page_loader.scripts.page_loader import download
-
-
-@pytest.mark.skip(reason='deativate')
-def test_download_check_content(requests_mock):
-    requests_mock.get('http://test.com', text='data')
-    with tempfile.TemporaryDirectory() as directory_name:
-        the_dir = pathlib.Path(directory_name)
-        with open(download('http://test.com', the_dir)) as f:
-            assert bs4.BeautifulSoup(f, 'lxml').get_text().strip() == 'data'
 
 
 def test_error_exit_code():
