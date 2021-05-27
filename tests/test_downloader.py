4f830f9cbdf3168e2a1b8b7d0eabb8b303352b98
ikievite <ikievite@gmail.com>
2021-05-27 11:14:16 +0300

    stats working with test for `download` function

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -3,7 +3,10 @@
 """Test downloader module."""
 
 
-from page_loader.downloader import replace_local_urls, find_local_resources
+import requests_mock
+import tempfile
+import pathlib
+from page_loader.downloader import replace_local_urls, find_local_resources, download
 
 
 expected_local_resources = [
@@ -33,3 +36,23 @@ def test_replace_local_urls():
                 'site-com-blog-about_files'
             )
     assert page_replaced == expected
+
+
+def test_download():
+    with open('tests/fixtures/site_com_content.txt') as f:
+        web_page = f.read()
+    with open('tests/fixtures/styles.css', 'rb') as f:
+        styles = f.read()
+    with open('tests/fixtures/me.jpg', 'rb') as f:
+        me_jpeg = f.read()
+    with open('tests/fixtures/scripts.js', 'rb') as f:
+        scripts = f.read()
+    with requests_mock.Mocker() as mock:
+        mock.get('http://site.com/blog/about', text=web_page)
+        mock.get('http://site.com/blog/about/assets/styles.css', content=styles)
+        mock.get('http://site.com/photos/me.jpg', content=me_jpeg)
+        mock.get('https://site.com/assets/scripts.js', content=scripts)
+        with tempfile.TemporaryDirectory() as directory_name:
+            the_dir = pathlib.Path(directory_name)
+            the_dir = 'tests/tests'
+            download('http://site.com/blog/about', the_dir)
