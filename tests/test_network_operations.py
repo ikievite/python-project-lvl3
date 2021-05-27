a55b27a94e3e12944cb304b9f6ebb8c26c56b490
ikievite <ikievite@gmail.com>
2021-05-27 16:49:54 +0300

    updated test_get_content_codes

--- tests/test_network_operations.py
+++ tests/test_network_operations.py
@@ -8,22 +8,18 @@ import logging
 import requests
 import requests_mock  # noqa: F401
 from page_loader.network_operations import get_content, format_url, HTML_EXTENSION
-from page_loader.errors import RequestError
+from page_loader.errors import RequestError, FileError
 
 
 logger = logging.getLogger(__name__)
 
-test_data = [
-    (404, '404 Client Error'),
-    (503, '503 Server Error'),
-]
+error_codes = [(404), (503)]
 
 
-@pytest.mark.skip()
-@pytest.mark.parametrize("code,description", test_data)
-def test_get_content_codes(requests_mock, code, description):  # noqa: F811
+@pytest.mark.parametrize("code", error_codes)
+def test_get_content_codes(requests_mock, code):  # noqa: F811
     requests_mock.get('http://test.com', status_code=code)
-    with pytest.raises(RequestError, match=description):
+    with pytest.raises(RequestError):
         get_content('http://test.com')
 
 
